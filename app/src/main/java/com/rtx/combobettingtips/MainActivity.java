21713223b00f08d811fdcd14dbb56ec6099d1ebc
Jeffrey Kimani <jeffrey.kimani@rooftopdynamics.com>
2021-04-06 01:23:50 +0300

    1.0 (1) Initial Store Release

--- app/src/main/java/com/rtx/combobettingtips/MainActivity.java
+++ app/src/main/java/com/rtx/combobettingtips/MainActivity.java
@@ -1,8 +1,6 @@
 package com.rtx.combobettingtips;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,16 +20,20 @@ import com.github.ybq.android.spinkit.sprite.Sprite;
 import com.github.ybq.android.spinkit.style.FoldingCube;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.rtdx.ads.BaseActivity;
+import com.rtdx.ads.configs.AdConfig;
 
 public class MainActivity extends BaseActivity {
 
     private WebView webView;
     SwipeRefreshLayout refreshLayout;
     ViewGroup progressLayout;
-    View noInternetLayout;
     Button reloadBtn;
+    private String postScript = "";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -100,18 +102,33 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onRefresh() {
                 refreshLayout.setRefreshing(true);
-                loadRemoteConfig();
+                fetchRemoteConfigs();
                 refreshLayout.setRefreshing(false);
             }
         });
 
-        noInternetLayout = findViewById(R.id.no_internet_layout);
-        reloadBtn = noInternetLayout.findViewById(R.id.reload_btn);
-        reloadBtn.setOnClickListener(view -> {
-            loadRemoteConfig();
-        });
+        if (isInternetConnected(this)) {
+            fetchRemoteConfigs();
+        } else {
+            showNoInternetDialog();
+        }
+    }
 
-        loadRemoteConfig();
+    private void showNoInternetDialog() {
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        if (root != null) {
+            View noInternetLayout = getLayoutInflater().inflate(R.layout.activity_no_connection, null);
+            root.addView(noInternetLayout);
+            Button reloadbtn = (Button) noInternetLayout.findViewById(R.id.reloadbtn);
+            reloadbtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (isInternetConnected(MainActivity.this)) {
+                        root.removeView(noInternetLayout);
+                        fetchRemoteConfigs();
+                    }
+                }
+            });
+        }
     }
 
     private void executePostScript() {
@@ -121,29 +138,20 @@ public class MainActivity extends BaseActivity {
     private void loadUrl(String url) {
         if (isInternetConnected(getApplicationContext())) {
             refreshLayout.setVisibility(View.GONE);
-            noInternetLayout.setVisibility(View.GONE);
             progressLayout.setVisibility(View.VISIBLE);
             webView.loadUrl(url);
         } else {
             progressLayout.setVisibility(View.GONE);
             refreshLayout.setVisibility(View.GONE);
-            noInternetLayout.setVisibility(View.VISIBLE);
             logMessage("Turn on Wifi or Mobile Data");
         }
         executePostScript();
     }
 
-    // Check Connection
-    public static boolean isInternetConnected(Context context) {
-        ConnectivityManager con_manager = (ConnectivityManager)
-                context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        return (con_manager.getActiveNetworkInfo() != null
-                && con_manager.getActiveNetworkInfo().isAvailable()
-                && con_manager.getActiveNetworkInfo().isConnected());
-    }
-
-    private void loadRemoteConfig() {
+    private void fetchRemoteConfigs() {
+        if (!isInternetConnected(this)) {
+            showNoInternetDialog();
+        }
 
         FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
@@ -157,72 +165,72 @@ public class MainActivity extends BaseActivity {
                         if (task.isSuccessful()) {
                             boolean updated = task.getResult();
 
-                            String adType = mFirebaseRemoteConfig.getString("ad_type");
                             String url = mFirebaseRemoteConfig.getString("url");
                             postScript = mFirebaseRemoteConfig.getString("post_script");
 
                             loadUrl(url);
-                            loadAd(adType);
+
+                            //Ads Stuff
+                            String adType = mFirebaseRemoteConfig.getString("ad_type").toLowerCase();
+                            if (BuildConfig.BUILD_DEBUG) {
+                                adType = mFirebaseRemoteConfig.getString("ad_type_test");
+                            }
+                            double adFilter = mFirebaseRemoteConfig.getDouble("ad_filter");
+                            logMessage("ADFILTER => " + adFilter + " | " + "Version Code => " + (double) BuildConfig.VERSION_CODE);
+
+                            if (adFilter >= (double) BuildConfig.VERSION_CODE || adType.equals("ad_type_test")) {
+                                fetchCollections(adType);
+                            }
 
                         } else {
                             Toast.makeText(MainActivity.this, "Fetch failed",
                                     Toast.LENGTH_SHORT).show();
-                            progressLayout.setVisibility(View.GONE);
-                            refreshLayout.setVisibility(View.GONE);
-                            noInternetLayout.setVisibility(View.VISIBLE);
+                            showNoInternetDialog();
                             logMessage("Turn on Wifi or Mobile Data");
                         }
                     }
                 });
     }
 
+    private void fetchCollections(String adType) {
+        FirebaseFirestore.getInstance().collection("ad_ids").document(adType).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                logMessage("DocumentSnapshot data: " + document.getData());
+                                AdConfig adConfig = new AdConfig(adType);
+                                adConfig.APP_ID = document.getString("APP_ID");
+                                adConfig.BANNER_ID = document.getString("BANNER_ID");
+                                adConfig.INTERSTITIAL_ID = document.getString("INTERSTITIAL_ID");
+                                adConfig.VIDEO_ID = document.getString("VIDEO_ID");
+                                adConfig.REWARDED_ID = document.getString("REWARDED_ID");
+                                adConfig.setAutoTestMode();
+                                logMessage(adConfig.toString());
+                                loadAd(adConfig);
+                            } else {
+                                logMessage("No such document => " + adType);
+                            }
+                        } else {
+                            logMessage("get failed with " + task.getException());
+                        }
+                    }
+                });
+    }
+
+    //Ads
     private boolean adsLoaded = false;
-    private String postScript = "";
 
-    private void loadAd(String adType) {
+    public void loadAd(AdConfig adConfig) {
         if (adsLoaded) return;
-        switch (adType.toLowerCase()) {
-            case "admob":
-                loadAdmob();
-                break;
-            case "facebook":
-                loadFacebook();
-                break;
-            case "adcolony":
-                loadAdColony();
-                break;
-            case "unity":
-                loadUnity();
-                break;
-            default:
-                //DO NOTHING
-                break;
-        }
+
+        adController.setAdConfig(adConfig);
+        adController.loadBannerAd();
+        adController.loadInterstitialAd(true);
+
         adsLoaded = true;
-    }
-
-    private void loadAdColony() {
-        adController.setAdConfig(AppConstants.ADCOLONY());
-        adController.loadBannerAd((ViewGroup) findViewById(R.id.ad_view));
-        adController.loadInterstitialAd(true);
-    }
-
-    private void loadFacebook() {
-        adController.setAdConfig(AppConstants.FACEBOOK());
-        adController.loadBannerAd((ViewGroup) findViewById(R.id.ad_view));
-        adController.loadInterstitialAd(true);
-    }
-
-    private void loadAdmob() {
-        adController.setAdConfig(AppConstants.ADMOB());
-        adController.loadBannerAd((ViewGroup) findViewById(R.id.ad_view));
-        adController.loadInterstitialAd(true);
-    }
-
-    private void loadUnity() {
-        adController.setAdConfig(AppConstants.UNITY());
-        adController.loadBannerAd((ViewGroup) findViewById(R.id.ad_view));
-        adController.loadInterstitialAd(true);
     }
 
     // functions for the action bar buttons
@@ -236,7 +244,7 @@ public class MainActivity extends BaseActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.rate_usbtnHM:
-                openStoreRatePage();
+                openRateDialog("#450148");
                 return true;
 
             case R.id.sharebtnHM:
